2f78f387aad6f8fb4dc4336fa79b7c43681306a7
idanhakimi <idanhakimi@gmail.com>
2018-11-22 19:40:44 +0200

    Fixed name

--- py/projects/functional_web_tests/tests/pages/home.py
+++ py/projects/functional_web_tests/tests/pages/home.py
@@ -8,9 +8,9 @@ class HomePage(Page):
         Page.__init__(self, driver, *args, **kwargs)
         self.login = Login()
 
-    def qaviton_menu_home_button(self):
+    def go_to_login(self):
         return self.find(locator.go_to_login)
 
     def navigate_to_Login(self, weight=10, *args, **kwargs):
-        self.qaviton_menu_home_button().click()
+        self.go_to_login().click()
         self.wait_until_page_loads()
