34481b75f289932521d43dcd23069cacc030f06c
idanhakimi <idanhakimi@gmail.com>
2018-11-22 16:25:37 +0200

    hot fix

--- py/projects/functional_web_tests/tests/execute_tests/login_test.py
+++ py/projects/functional_web_tests/tests/execute_tests/login_test.py
@@ -1,3 +1,3 @@
 def test_login(app):
-    app.navigate(app.qaviton_home.login)
-    app.qaviton_home.login(usename="idan", password="idan")
+    app.navigate(app.home.login)
+    app.home.login(usename="idan", password="idan")
