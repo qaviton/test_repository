4eb62e9645d6796051d0c919dd890900cd74b4a4
idanhakimi <idanhakimi@gmail.com>
2018-12-26 16:48:07 +0200

    New Tests

---
+++ py/projects/functional_web_tests/tests/execute_tests/login/login_with_missing_fields.py
@@ -0,0 +1,19 @@
+def login_empty_username_test(app):
+    app.navigate(app.home.login)
+    app.home.login.error(username="", password="idan", error="missing username")
+
+def login_empty_email_test(app):
+    app.navigate(app.home.login)
+    app.home.login.error(email="", password="idan", error="missing email")
+
+def login_username_empty_password_test(app):
+    app.navigate(app.home.login)
+    app.home.login.error(username="idan", password="", error="missing password")
+
+def login_email_empty_password_test(app):
+    app.navigate(app.home.login)
+     app.home.login.error(email="idanhakimi@gmail.com", password="", error="missing password")
+
+
+
+#Done
