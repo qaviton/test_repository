245b67d4825c283b6bab0219162bcf3e2f5ce546
idanhakimi <idanhakimi@gmail.com>
2019-02-28 13:46:03 +0200

    New signup tests

--- py/projects/functional_web_tests/tests/execute_tests/signup/sign_up.py
+++ py/projects/functional_web_tests/tests/execute_tests/signup/sign_up.py
@@ -28,3 +28,13 @@ def test_regular_signup_no_navbar(app):
     app.navigate(app.home.signup)
     app.home.signup(username="sdsdsdsd@gmail.com", password="sdsdsdtterssd1239")
 
+##regular signup with robot
+
+def test_regular_signup_with_robot_and_navbar(app):
+    app.navigate(app.home.navbar)
+    app.navigate(app.home.signup)
+    app.home.signup.sign_up_with_robot(username="sdsdsdsd@gmail.com", password="sdsdsdtterssd1239")
+
+def test_regular_signup_with_robot_no_navbar(app):
+    app.navigate(app.home.signup)
+    app.home.signup.sign_up_with_robot(username="sdsdsdsd@gmail.com", password="sdsdsdtterssd1239")
